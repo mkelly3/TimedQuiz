ffa0741cb4ff756c1453f6682af4872f5caab478
Morgan Kelly <mkelly5380@gmail.com>
2022-10-01 17:12:52 -0500

    When the first button is clicked it moves onto the next question

--- script.js
+++ script.js
@@ -19,7 +19,39 @@ var questionChoices = [
         choice2: "<a/>",
         choice3: "<img/>",
         choice4: "<br/>",
-        correct: "choice3"
+        correct: "one"
+    },
+    {
+        question: "What is my name",
+        choice1: "Morgan",
+        choice2: "Kelly",
+        choice3: "Liz",
+        choice4: "All of the above",
+        correct: "four"
+    },
+    {
+        question: "What is my favorite color",
+        choice1: "blue",
+        choice2: "green",
+        choice3: "purple",
+        choice4: "yellow",
+        correct: "one"
+    },
+    {
+        question: "Hello word",
+        choice1: "<picture/>",
+        choice2: "<a/>",
+        choice3: "<img/>",
+        choice4: "<br/>",
+        correct: "three"
+    },
+    {
+        question: "What is the meaning of life",
+        choice1: "IDK",
+        choice2: "Everything",
+        choice3: "Ice Cream",
+        choice4: "Hola",
+        correct: "choice2"
     },
     {
         question: "What HTML element is used to insert a picture",
@@ -27,165 +59,133 @@ var questionChoices = [
         choice2: "<a/>",
         choice3: "<img/>",
         choice4: "<br/>",
-        correct: "choice3"
+        correct: "one"
     },
     {
-        question: "What HTML element is used to insert a picture",
-        choice1: "<picture/>",
-        choice2: "<a/>",
-        choice3: "<img/>",
-        choice4: "<br/>",
-        correct: "choice3"
+        question: "What is 2+2",
+        choice1: "4",
+        choice2: "6",
+        choice3: "2",
+        choice4: "1",
+        correct: "one"
     },
     {
-        question: "What HTML element is used to insert a picture",
-        choice1: "<picture/>",
-        choice2: "<a/>",
-        choice3: "<img/>",
-        choice4: "<br/>",
-        correct: "choice3"
+        question: "What is my favorite animal",
+        choice1: "Tiger",
+        choice2: "Dog",
+        choice3: "Dolphin",
+        choice4: "Bear",
+        correct: "two"
     },
     {
-        question: "What HTML element is used to insert a picture",
-        choice1: "<picture/>",
-        choice2: "<a/>",
-        choice3: "<img/>",
-        choice4: "<br/>",
-        correct: "choice3"
-    },
+        question: "How many siblings do i have",
+        choice1: "three",
+        choice2: "zero",
+        choice3: "2",
+        choice4: "1",
+        correct: "four"
+    }
+    ,
     {
-        question: "What HTML element is used to insert a picture",
-        choice1: "<picture/>",
-        choice2: "<a/>",
-        choice3: "<img/>",
-        choice4: "<br/>",
-        correct: "choice3"
-    },
-    {
-        question: "What HTML element is used to insert a picture",
-        choice1: "<picture/>",
-        choice2: "<a/>",
-        choice3: "<img/>",
-        choice4: "<br/>",
-        correct: "choice3"
-    },
-    {
-        question: "What HTML element is used to insert a picture",
-        choice1: "<picture/>",
-        choice2: "<a/>",
-        choice3: "<img/>",
-        choice4: "<br/>",
-        correct: "choice3"
-    },
-    {
-        question: "What HTML element is used to insert a picture",
-        choice1: "<picture/>",
-        choice2: "<a/>",
-        choice3: "<img/>",
-        choice4: "<br/>",
-        correct: "choice3"
-    },
-    {
-        question: "What HTML element is used to insert a picture",
-        choice1: "<picture/>",
-        choice2: "<a/>",
-        choice3: "<img/>",
-        choice4: "<br/>",
-        correct: "choice3"
-    },
-    {
-        question: "What HTML element is used to insert a picture",
-        choice1: "<picture/>",
-        choice2: "<a/>",
-        choice3: "<img/>",
-        choice4: "<br/>",
-        correct: "choice3"
-    },
-    {
-        question: "What HTML element is used to insert a picture",
-        choice1: "<picture/>",
-        choice2: "<a/>",
-        choice3: "<img/>",
-        choice4: "<br/>",
-        correct: "choice3"
-    },
-    {
-        question: "What HTML element is used to insert a picture",
-        choice1: "<picture/>",
-        choice2: "<a/>",
-        choice3: "<img/>",
-        choice4: "<br/>",
-        correct: "choice3"
-    },
-    {
-        question: "What HTML element is used to insert a picture",
-        choice1: "<picture/>",
-        choice2: "<a/>",
-        choice3: "<img/>",
-        choice4: "<br/>",
-        correct: "choice3"
-    },
-    {
-        question: "What HTML element is used to insert a picture",
-        choice1: "<picture/>",
-        choice2: "<a/>",
-        choice3: "<img/>",
-        choice4: "<br/>",
-        correct: "choice3"
-    },
-    {
-        question: "What HTML element is used to insert a picture",
-        choice1: "<picture/>",
-        choice2: "<a/>",
-        choice3: "<img/>",
-        choice4: "<br/>",
-        correct: "choice3"
-    },
-    {
-        question: "What HTML element is used to insert a picture",
-        choice1: "<picture/>",
-        choice2: "<a/>",
-        choice3: "<img/>",
-        choice4: "<br/>",
-        correct: "choice3"
-    },
-    {
-        question: "What HTML element is used to insert a picture",
-        choice1: "<picture/>",
-        choice2: "<a/>",
-        choice3: "<img/>",
-        choice4: "<br/>",
-        correct: "choice3"
-    },
-    {
-        question: "What HTML element is used to insert a picture",
-        choice1: "<picture/>",
-        choice2: "<a/>",
-        choice3: "<img/>",
-        choice4: "<br/>",
-        correct: "choice3"
-    },
-    {
-        question: "What HTML element is used to insert a picture",
-        choice1: "<picture/>",
-        choice2: "<a/>",
-        choice3: "<img/>",
-        choice4: "<br/>",
-        correct: "choice3"
-    },
+        question: "What is my favorite season",
+        choice1: "fall",
+        choice2: "spring",
+        choice3: "winter",
+        choice4: "summer",
+        correct: "three"
+    }
+    //,
+    // {
+    //     question: "What HTML element is used to insert a picture",
+    //     choice1: "<picture/>",
+    //     choice2: "<a/>",
+    //     choice3: "<img/>",
+    //     choice4: "<br/>",
+    //     correct: "one"
+    // },
+    // {
+    //     question: "What HTML element is used to insert a picture",
+    //     choice1: "<picture/>",
+    //     choice2: "<a/>",
+    //     choice3: "<img/>",
+    //     choice4: "<br/>",
+    //     correct: "one"
+    // },
+    // {
+    //     question: "What HTML element is used to insert a picture",
+    //     choice1: "<picture/>",
+    //     choice2: "<a/>",
+    //     choice3: "<img/>",
+    //     choice4: "<br/>",
+    //     correct: "one"
+    // },
+    // {
+    //     question: "What HTML element is used to insert a picture",
+    //     choice1: "<picture/>",
+    //     choice2: "<a/>",
+    //     choice3: "<img/>",
+    //     choice4: "<br/>",
+    //     correct: "one"
+    // },
+    // {
+    //     question: "What HTML element is used to insert a picture",
+    //     choice1: "<picture/>",
+    //     choice2: "<a/>",
+    //     choice3: "<img/>",
+    //     choice4: "<br/>",
+    //     correct: "one"
+    // },
+    // {
+    //     question: "What HTML element is used to insert a picture",
+    //     choice1: "<picture/>",
+    //     choice2: "<a/>",
+    //     choice3: "<img/>",
+    //     choice4: "<br/>",
+    //     correct: "one"
+    // },
+    // {
+    //     question: "What HTML element is used to insert a picture",
+    //     choice1: "<picture/>",
+    //     choice2: "<a/>",
+    //     choice3: "<img/>",
+    //     choice4: "<br/>",
+    //     correct: "one"
+    // },
+    // {
+    //     question: "What HTML element is used to insert a picture",
+    //     choice1: "<picture/>",
+    //     choice2: "<a/>",
+    //     choice3: "<img/>",
+    //     choice4: "<br/>",
+    //     correct: "one"
+    // },
+    // {
+    //     question: "What HTML element is used to insert a picture",
+    //     choice1: "<picture/>",
+    //     choice2: "<a/>",
+    //     choice3: "<img/>",
+    //     choice4: "<br/>",
+    //     correct: "one"
+    // },
+    // {
+    //     question: "What HTML element is used to insert a picture",
+    //     choice1: "<picture/>",
+    //     choice2: "<a/>",
+    //     choice3: "<img/>",
+    //     choice4: "<br/>",
+    //     correct: "one"
+    // },
 ]
 
 //empty array to hold random questions
 var randomQuestions = []
+console.log(randomQuestions);
 
 var questionNumber = 0; 
 
 
 function setTimer() {
-    // Sets interval in variable
-    //The setInterval() method takes a function as its first argument
     var timerInterval = setInterval(function() {
-      //The function is the action that we want to be evaluated at each interval. This function prints a message at each interval
       time--;
       timeEl.textContent = "Time: " + time;
   
@@ -203,8 +203,6 @@ function setTimer() {
         startButton.setAttribute("style","font-size:20px");
 
     });
-    
-
   }
 
   onstartButton();
@@ -220,20 +218,18 @@ function setTimer() {
         //if the question has not already been randomly selected it will be pushed to the array
         if(!randomQuestions.includes(random)){
             randomQuestions.push(random);
-            console.log(randomQuestions);
+            //console.log(randomQuestions);
         }
     }
   }
 
-
-
-
   //display the next question and answer choices
+  var currentQ;
 
   function displayQestion(number){
     chooseRandomQuestion();
-
-    var currentQ = randomQuestions[number];
+    currentQ = randomQuestions[number]
+    console.log(questionNumber)
     questionTitle.textContent = currentQ.question;
     answerChoice1.textContent = currentQ.choice1;
     answerChoice2.textContent = currentQ.choice2;
@@ -242,5 +238,29 @@ function setTimer() {
 
   }
 
+  //function to check if the correct answer is chosen
+  
   displayQestion(questionNumber);
+  function checkAnswer() {
+    var correctAnswer = currentQ.correct;
+    answerChoice1.addEventListener("click",function(event){
+        key = event.target.id;
+        if(key == "one"){
+            questionNumber++
+            score++
+        }
+        else{
+            time = time - 1
+            console.log(time)
 
+        }
+        displayQestion(questionNumber);
+
+    })
+
+  }
+  
+  checkAnswer();
+  
+
+  
